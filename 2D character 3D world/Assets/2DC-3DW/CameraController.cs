6df5c33248dad921d8ab9930edc915594c7d7880
Danielevel <daniel.es.guarin@gmail.com>
2023-02-06 04:58:56 -0500

    Uploading the proyect to main
    uploading the project in itself, read the code of "Alpha_2D_in_3d_world.cs" to understand the logic of it, even tho I made the interface of the code in the asset manager very simple to understand, you can use both images and animations in this proyect. have fun owo)=b

---
+++ 2D character 3D world/Assets/2DC-3DW/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+		public Transform target;
+		public float angleY = 35;
+		public bool InvertY;
+		public float rotationSmoothing = 10;
+		public float rotationSensitivity = 7;
+		public float distance = 10;
+		public float VerticalRotLimit = 40;
+
+		private Vector3 _angle = new Vector3();
+		private Quaternion _oldRotation = new Quaternion();
+
+		private Transform _t;
+
+		public Vector2 CurrentRotation { get { return _angle; } }
+
+		void Start()
+		{
+			_t = transform;
+			_oldRotation = _t.rotation;//asigna la rotacion de la camara
+			_angle.y = angleY;
+		}
+
+		public void ClampAngle(ref Vector3 angle)
+		{
+			if(angle.x < -180) angle.x += 360;
+			else if(angle.x > 180) angle.x -= 360;
+
+			if(angle.y < -VerticalRotLimit) angle.y = -VerticalRotLimit;
+			else if(angle.y > VerticalRotLimit) angle.y = VerticalRotLimit;
+
+			if(angle.z < -180) angle.z += 360;
+			else if(angle.z > 180) angle.z -= 360;
+		}
+
+		void Update()
+		{
+			if(target && Input.GetMouseButton(1))
+			{
+				_angle.x += Input.GetAxis("Mouse X") * rotationSensitivity;
+
+				if (InvertY){
+				  _angle.y -= Input.GetAxis("Mouse Y") * rotationSensitivity;
+				}else{
+				  _angle.y += Input.GetAxis("Mouse Y") * rotationSensitivity;
+				}
+				
+				ClampAngle(ref _angle);
+				
+			}/*else{
+				_angle.x += Input.GetAxis("Horizontal2")* rotationSensitivity;
+				_angle.y += Input.GetAxis("Vertical2")* rotationSensitivity;
+				ClampAngle(ref _angle);
+			}*/
+					 
+				
+		}
+
+		void LateUpdate()
+		{
+			if(target)
+			{
+				Quaternion angleRotation = Quaternion.Euler(_angle.y, _angle.x, 0);
+				Quaternion currentRotation = Quaternion.Lerp(_oldRotation, angleRotation, Time.deltaTime * rotationSmoothing);
+
+				_oldRotation = currentRotation;
+
+				_t.position = target.position - currentRotation * Vector3.forward * distance;
+				_t.LookAt(target.position, Vector3.up);
+			}
+		}
+	
+}
---
+++ 2D character 3D world/Assets/2DC-3DW/thirdPersonMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;                  // A reference to the main camera in the scenes transform
+    public Vector3 moveDir;
+    public Vector3 movement;
+
+    [Header ("Gravity")]
+    public float gravity = 10;
+    public float constantGravity = 0.6f;
+    public float maxGravity = -15;
+
+    private float currentGravity;
+    private Vector3 gravityDirection = Vector3.down;
+    private Vector3 gravityMovement;
+
+    //public Rigidbody rb;
+    public Animator animator; 
+
+     private void Start()
+        {
+            //rb = GetComponent<Rigidbody>();
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+            }
+        }
+
+    private bool IsGrounded()
+    {
+        return controller.isGrounded;
+    }
+
+    private void CalculateGravity()
+    {
+        if (IsGrounded())
+        {
+            currentGravity = constantGravity;
+        }else
+        {
+            if(currentGravity > maxGravity)
+            {
+                currentGravity -= gravity * Time.deltaTime;
+            }
+        }
+        gravityMovement = gravityDirection * -currentGravity * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateGravity();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+        
+                
+        if(direction.magnitude >= 0.1f) 
+        {
+            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f)*Vector3.forward;
+            movement = moveDir.normalized * speed * Time.deltaTime;
+            controller.Move(movement);
+            animator.SetFloat("speed",controller.velocity.magnitude);// this is just for animation
+        }else{
+            animator.SetFloat("speed",controller.velocity.magnitude);
+        }
+        controller.Move(gravityMovement);
+        
+         //rigidbody.velocity.magnitude
+        
+    }
+
+    
+}
